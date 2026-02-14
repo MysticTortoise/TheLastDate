0f2c3cd665bb33cf95a287b57a3c38ea982ef16e
ocfrey <ocfrey270@gmail.com>
2026-02-14 13:14:20 -0700

    Library Minigame works now

---
+++ Assets/Scripts/Minigames/LibraryMinigame/WordGameManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordGameManager : MonoBehaviour
+{
+    private WordGraph graph;
+    [HideInInspector] public List<Button> buttonsClicked;
+    [HideInInspector] public string wordString;
+    private int score;
+    [SerializeField] private TextMeshProUGUI scoreTextBox;
+    [SerializeField] private TextAsset words;
+    private HashSet<string> dictionary;
+    void Start()
+    {
+        dictionary = new();
+        string[] lines = words.text.Split("\n");
+        foreach(string line in lines)
+        {
+            dictionary.Add(line);
+        }
+        buttonsClicked = new();
+        wordString = "";
+        score = 0;
+        graph = GetComponentInChildren<WordGraph>();
+        
+    }
+
+    public void OnClick(Button button)
+    {
+        if ((buttonsClicked.Count == 0) || (graph.adgacencyGraph[buttonsClicked[buttonsClicked.Count - 1].name].Contains(button) && !buttonsClicked.Contains(button)))
+        {
+            button.GetComponent<Image>().color = Color.green;
+            wordString += button.GetComponentInChildren<TextMeshProUGUI>().text;
+            buttonsClicked.Add(button);
+        }
+        else if (buttonsClicked[buttonsClicked.Count - 1] == button)
+        {
+            button.GetComponent<Image>().color = Color.grey;
+            wordString = wordString.Substring(0, wordString.Length - 1);
+            buttonsClicked.Remove(button);
+        }
+    }
+
+    public void SubmitWord()
+    {
+        if (dictionary.Contains(wordString))
+        {
+           score += wordString.Length;
+        }
+        graph.OnRegen();
+        wordString = "";
+        scoreTextBox.text = "Score: " + score;
+    }
+}
---
+++ Assets/Scripts/Minigames/LibraryMinigame/WordGraph.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordGraph : MonoBehaviour
+{
+    private Button[] wordButtons;
+    [HideInInspector] public Dictionary<string, LinkedList<Button>> adgacencyGraph;
+    [SerializeField] private WordGameManager manager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GenerateBoard();
+    }
+
+    private void GenerateBoard()
+    {
+        manager.wordString = "";
+        manager.buttonsClicked = new();
+        adgacencyGraph = new();
+        wordButtons = GetComponentsInChildren<Button>();
+        foreach (Button button in wordButtons)
+        {
+            button.GetComponentInChildren<TextMeshProUGUI>().text = GetRandomLetter().ToString();
+            button.GetComponent<Image>().color = Color.grey;
+            adgacencyGraph.Add(button.name, new());
+        }
+
+        foreach(Button button in wordButtons)
+        {
+            foreach(Button otherButton in wordButtons)
+            {
+                if ((Math.Abs(button.transform.position.y - otherButton.transform.position.y) <= 64) && (Math.Abs(button.transform.position.x - otherButton.transform.position.x) <= 75) && (otherButton != button))
+                {
+                    adgacencyGraph[button.name].AddLast(otherButton);
+                }
+            }
+        }
+    }
+
+    private static char GetRandomLetter()
+    {
+        return (char) UnityEngine.Random.Range(65, 91);
+    }
+
+    public void OnRegen()
+    {
+        GenerateBoard();
+    }
+}
