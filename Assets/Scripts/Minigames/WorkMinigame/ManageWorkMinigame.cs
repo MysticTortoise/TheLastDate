32d3c8a0d2c42f68d9643bc0ae08026e59753ecb
ocfrey <ocfrey270@gmail.com>
2026-02-13 20:52:28 -0700

    papers please minigame works

--- Assets/Scripts/Minigames/WorkMinigame/ManageWorkMinigame.cs
+++ Assets/Scripts/Minigames/WorkMinigame/ManageWorkMinigame.cs
@@ -1,57 +1,58 @@
 using System.Collections.Generic;
 using TMPro;
+using Unity.Collections;
 using Unity.VisualScripting;
-using UnityEngine;
-
+using UnityEngine; 
 public class ManageWorkMinigame : MonoBehaviour
 {
     [SerializeField] private List<EmployeeProfile> employees;
-    private int employeeIndex;
     private int score;
     [SerializeField] private TextMeshProUGUI nameTextBox;
     [SerializeField] private TextMeshProUGUI ageTextBox;
     [SerializeField] private TextMeshProUGUI positionTextBox;
     [SerializeField] private TextMeshProUGUI yearsExperienceTextBox;
     [SerializeField] private TextMeshProUGUI descriptionTextBox;
+    public int employeeIndex;
     void Start()
     {
-        employees = new();
-        employeeIndex = 0;
-        nameTextBox.text = employees[0].employeeName;
-        ageTextBox.text = employees[0].age;
-        positionTextBox.text = employees[0].position;
-        yearsExperienceTextBox.text = employees[0].yearsExperience;
-        descriptionTextBox.text = employees[0].behaviorDescription;
-    }
-
-    public void OnFire()
-    {
-        score += employees[employeeIndex].fireScore;
-        Debug.Log("Empathy Lost" + employees[employeeIndex].fireScore);
-        employeeIndex++;
-        if (employeeIndex < employees.Count)
-        {
+        employeeIndex = Random.Range(0, employees.Count);
         nameTextBox.text = employees[employeeIndex].employeeName;
         ageTextBox.text = employees[employeeIndex].age;
         positionTextBox.text = employees[employeeIndex].position;
         yearsExperienceTextBox.text = employees[employeeIndex].yearsExperience;
         descriptionTextBox.text = employees[employeeIndex].behaviorDescription;
+    }
+
+    public void OnFire()
+    {
+        if (employees.Count > 0)
+        {
+        employeeIndex = Random.Range(0, employees.Count);
+        nameTextBox.text = employees[employeeIndex].employeeName;
+        ageTextBox.text = employees[employeeIndex].age;
+        positionTextBox.text = employees[employeeIndex].position;
+        yearsExperienceTextBox.text = employees[employeeIndex].yearsExperience;
+        descriptionTextBox.text = employees[employeeIndex].behaviorDescription;
+        score += employees[employeeIndex].fireScore;
+        Debug.Log("Empathy Lost" + employees[employeeIndex].fireScore);
+        employees.RemoveAt(employeeIndex);
         }
         else {EndGame();}
     }
 
     public void OnKeep()
     {
-        score += employees[employeeIndex].maintainScore;
-        Debug.Log("Empathy Gained" + employees[employeeIndex].fireScore);
-        employeeIndex++;
-        if (employeeIndex < employees.Count)
+        if (employees.Count > 0)
         {
+        employeeIndex = Random.Range(0, employees.Count);
         nameTextBox.text = employees[employeeIndex].employeeName;
         ageTextBox.text = employees[employeeIndex].age;
         positionTextBox.text = employees[employeeIndex].position;
         yearsExperienceTextBox.text = employees[employeeIndex].yearsExperience;
         descriptionTextBox.text = employees[employeeIndex].behaviorDescription;
+        score += employees[employeeIndex].maintainScore;
+        Debug.Log("Empathy Gained" + employees[employeeIndex].maintainScore);
+        employees.RemoveAt(employeeIndex);
         }
         else {EndGame();}
     }
