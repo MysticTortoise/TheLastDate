391570008176a7185eb1f4664ec6c1e23ca3603f
MysticTortoise <shadowknight7788@gmail.com>
2026-02-16 14:41:41 -0700

    ending shiz done

---
+++ Assets/Scripts/EndScreenManager.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreenManager : MonoBehaviour
+{
+    [SerializeField] private GameObject DateScene;
+
+    public void GoBack()
+    {
+        PlayerGlobalHandler.GlobalHandler.stats.charm = 0;
+        PlayerGlobalHandler.LoadIntoMainGame(DateScene);
+    }
+
+    public void TitleScreen()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
--- Assets/Scripts/PlayerGlobalHandler.cs
+++ Assets/Scripts/PlayerGlobalHandler.cs
@@ -8,12 +8,12 @@ public class PlayerGlobalHandler : MonoBehaviour
 {
     public StatBlock stats = new StatBlock
     {
-        empathy = 0,
+        empathy = -5,
         charm = 0,
-        looks = 0,
-        money = 0,
-        rizz = 0,
-         smarts = 5,
+        looks = -5,
+        money = -5,
+        rizz = -5,
+         smarts = -5,
     };
     private TimerHandler timerHandler;
     [NonSerialized] public List<ItemDefinition> heldItems = new();
