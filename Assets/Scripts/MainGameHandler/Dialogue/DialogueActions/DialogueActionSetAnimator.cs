9619cbf482e98214a94264014585e34cd6ddf638
MysticTortoise <shadowknight7788@gmail.com>
2026-02-16 14:06:15 -0700

    grunt stuff

---
+++ Assets/Scripts/MainGameHandler/Dialogue/DialogueActions/DialogueActionSetAnimator.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AnimateAction", menuName = "Dialogue/Actions/Set Animator Int")]
+public class DialogueActionSetAnimator : DialogueActionBase
+{
+    public string AnimatorTag;
+    public string Name;
+    public int Value;
+    
+    public override void Trigger()
+    {
+        GameTag.GetFirstObjectWith(AnimatorTag).GetComponent<Animator>().SetInteger(Name, Value);
+    }
+}
