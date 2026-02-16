f41e60f42084be726f535ff8c151b840a836d076
MysticTortoise <shadowknight7788@gmail.com>
2026-02-16 10:23:02 -0700

    add item action

---
+++ Assets/Scripts/MainGameHandler/Dialogue/DialogueActions/DIalogueActionGiveItem.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GiveItem", menuName = "Dialogue/Actions/Give Item")]
+public class DIalogueActionGiveItem : DialogueActionBase
+{
+    public ItemDefinition Item;
+
+    public override void Trigger()
+    {
+        PlayerGlobalHandler.GlobalHandler.heldItems.Add(Item);
+    }
+}
