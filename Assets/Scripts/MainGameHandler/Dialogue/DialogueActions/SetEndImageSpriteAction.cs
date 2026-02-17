2818909d1c105887eadf248609258b4f2ec871a9
MysticTortoise <shadowknight7788@gmail.com>
2026-02-16 18:19:39 -0700

    fix stats

--- Assets/Scripts/MainGameHandler/Dialogue/DialogueActions/SetEndImageSpriteAction.cs
+++ Assets/Scripts/MainGameHandler/Dialogue/DialogueActions/SetEndImageSpriteAction.cs
@@ -9,6 +9,15 @@ public class SetEndImageSpriteAction : DialogueActionBase
 
     public override void Trigger()
     {
+        PlayerGlobalHandler.GlobalHandler.stats = new StatBlock
+        {
+            empathy = 0,
+            charm = 0,
+            looks = 0,
+            money = 0,
+            rizz = 0,
+            smarts = 0,
+        };
         EndScreenManager.EndBGSprite = Image;
     }
 }
